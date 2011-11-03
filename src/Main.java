49848b733e298bce8e443c7e38e23d9242415f86
Fredrik Lundin <lundin_fredrik@hotmail.com>
2011-11-03 13:00:14 +0100

    kommentar

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 
 public class Main {
-
+	//commit test!
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Solaris");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
